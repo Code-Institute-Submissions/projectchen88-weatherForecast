4d14fe1689f7adb3b2a0f6cf2148fc1728949527
Ubuntu <ubuntu@ip-172-31-91-39.ec2.internal>
2020-04-27 19:40:41 +0000

    added 2nd set of data

--- scripts/script.js
+++ scripts/script.js
@@ -1,47 +1,44 @@
 /*  global $ axios */
 
-var dataSource = "https://api.data.gov.sg/v1/environment/2-hour-weather-forecast"
-var data =[]
+var source1 = "https://api.data.gov.sg/v1/environment/2-hour-weather-forecast"
+var source2 = "https://api.data.gov.sg/v1/environment/4-day-weather-forecast"
+var forecastData, tempData =[]
 var option =[]
 var status
 
 
 //AXIO TO GET DATA FROM JSON FILE (local)
-function getData(callback) {
-  axios.get(dataSource)
+function getForecast(callback) {
+  axios.get(source1)
     .then(function(response) {
       let result = response.data;
+      // console.log(result)
+      callback(result)
+    })
+}
+function getTemp(callback) {
+  axios.get(source2)
+    .then(function(response) {
+      let result = response.data.items[0].forecasts;
       console.log(result)
       callback(result)
     })
 }
 
-getData( function(data) {
-    status = data.api_info.status
+getForecast( function(forecastData) {
+    status = forecastData.api_info.status
     document.getElementById("status").innerHTML = status;
 
-    let forecasts = data.items[0].forecasts
-    document.getElementById("area").innerHTML = forecasts[0].area;
+    let forecastsArea = forecastData.items[0].forecasts
 
     var select = document.getElementById("select"); 
-    for (let item in forecasts) {
+    for (let item in forecastsArea) {
       var el = document.createElement("option");
-      el.textContent = forecasts[item].area;
-      el.value = forecasts[item].area;
+      el.textContent = forecastsArea[item].area;
+      el.value = forecastsArea[item].area;
       select.appendChild(el);
     }
-    
-    console.log(forecasts)
+    getTemp(function (tempData) {
+    })
 })
 
-
-
-// $ (function() {
-  
-  
-//   $('#select').empty();
-//   $.each(option, function(i, p) {
-//       $('#select').append($('<option></option>').val(p).html(p));
-//   });
-  
-// })
